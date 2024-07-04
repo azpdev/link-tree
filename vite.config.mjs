1efdf8f333d4fe511088493590d7c3be873d62c3
Alex Palomin <alexpalomin21@gmail.com>
2024-07-04 14:14:19 -0600

    New Vite config.

---
+++ vite.config.mjs
@@ -0,0 +1,20 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import htmlMinifier from "vite-plugin-html-minifier";
+
+export default defineConfig({
+  appType: "mpa",
+  base: "",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+  plugins: [
+    htmlMinifier({
+      minify: true,
+    }),
+  ],
+});
